618ca04a2def9fd01f57e36920c0b56e82fae55c
Stan Haakman <stanhaakman@icloud.com>
2018-12-17 12:10:26 +0100

    update 17 dec

--- curlsys.php
+++ curlsys.php
@@ -10,12 +10,34 @@ include 'navbar.php';
 
     </div>
 
-    <div class="outline-img">
+    <div id="outline-img">
         
     </div>
     </div>
+
+    <div class="content">
+    
+    </div>
 </main>
 
+<script>
+    window.onload = function () {
+
+        var foto = document.getElementById('outline-img');
+
+    function changeImage() {   
+        var BackgroundImg = ["images/1x.png",
+            "images/2x.png",
+            "images/3x.png",
+            "images/4x.png"
+        ];
+        var i = Math.floor((Math.random() * 4));
+        foto.style.backgroundImage = 'url("' + BackgroundImg[i] + '")';
+
+    }
+    window.setInterval(changeImage, 3000);
+}
+</script>
 
 <?php
 include 'footer.php'
