d8e454c3d248548fc681a9cbfd49d88ef1a637c0
maessmit <25165@ma-web.nl>
2019-01-11 11:24:12 +0100

    contact.php

--- contact.php
+++ contact.php
@@ -2,17 +2,15 @@
 <?php include 'navbar.php'?>
     <div class="contactform">
 
-        <h1 class="brand">Neem contact op</h1>
+        <h1 class="brand">Contact Mij</h1>
             <div class="wrapper">
                 <div class="company-info">
-                    <h3 id="groot-contact">Wilt u snel contact, dan graag bellen</h3>
+                    <h3>Contact Mij</h3>
                     <ul>
-                        <!-- <li id="groot-contact">Wilt u snel contact, dan graag bellen</li> -->
-                        <li>Adres:</li>
-                        <li>Piraat 12</li>
-                        <li>1671 RX Medemblik </li>
+                        <li>Straat: 12 Piraat</li>
+                        <li>Adres: 1671 RX   Medemblik </li>
                         <li>Telefoon (0227) 724544 </li>
-                        <li>Mail: <a id="a-contact-mail" href="mailto:info@dehaarpiraat.nl">info@dehaarpiraat.nl</a></li>
+                        <li>Mail: mirellahaakman@hotmail.com</li>
                         <br>
                         <li>Maandag:                    08:30 - 17:30</li>
                         <li>Dinsdag:                         Gesloten</li>
@@ -58,17 +56,18 @@
                 </div>
             </div>
     </div>
-
+<div class="google-maps">
     <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d9491.52932155905!2d5.099476839083594!3d52.761788722690106!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x82b57f8b33e81f37!2sDe+HaarPiraat!5e0!3m2!1snl!2snl!4v1543491225228"
-            class="map" frameborder="0" allowfullscreen></iframe>
+            frameborder="0" allowfullscreen>
 
+    </iframe>
+</div>
+
+<div class="google-360">
     <iframe class="framebox inherit-locale Go"
-            style=""
             src="https://www.google.com/maps/embed?pb=!4v1541593605780!6m8!1m7!1sCAoSLEFGMVFpcE9Lc0Z3RlpNclljajlhYVFyVVQxb2dyaGMwM1FFRWJIR0V0NGVE!2m2!1d52.761783875067!2d5.099458182356!3f112.78530989877571!4f-6.884206240899701!5f0.7820865974627469"
             id="google_maps--view">
     </iframe>
+</div>
 
-
-<?php 
-include 'footer.php'
-?>
+<?php include 'footer.php'?>
