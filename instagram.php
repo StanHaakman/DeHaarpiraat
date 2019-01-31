e0b35ac8fd977a91fd45ef3f9e1d6aa6fb5949b5
25173 <25173@ma-web.nl>
2019-01-31 11:59:41 +0100

    instagram bijgewerkt
    de gridlayout bijgewerkt

--- instagram.php
+++ instagram.php
@@ -2,14 +2,12 @@
 require( 'header.php' );
 require( 'navbar.php' );
 ?>
+
     <div id="instaProfile"></div>
 
+    <div id="instafeed" class="grid"></div>
 
-<div id="instafeed"></div>
-
-    <br>
     <button id="btn-instafeed-load" class="btn">Load more</button>
-
 <?php
 require( 'footer.php' );
 ?>
--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,6 @@
 let accesstokenHaarpiraat = '6258864564.1677ed0.58537fe7721f487286b7f72483ab7113';
 let useridhaarpiraat = '6258864564';
+
 $(document).ready(function() {
 
     let btnInstafeedLoad = document.getElementById("btn-instafeed-load");
@@ -8,7 +9,7 @@ $(document).ready(function() {
         get: 'user',
         userId: useridhaarpiraat, // de userid of the account
         target: "instafeed", // where the data will be sent
-        limit: 8,  // the number of pictures that we get
+        limit: 12,  // the number of pictures that we get
         /*
         the size of the picture
         */
@@ -38,7 +39,7 @@ $(document).ready(function() {
         //     }
         //     return true;
         // },
-        template: '<header>{{model.template}}</header><footer><a href="http://instagram.com/{{model.user.username}}">@{{model.user.username}}</a><br>{{caption}}</footer>',
+        // template: '<header>{{model.template}}</header><footer><a href="http://instagram.com/{{model.user.username}}">@{{model.user.username}}</a><br>{{caption}}</footer>',
 
 
         /*
@@ -50,16 +51,19 @@ $(document).ready(function() {
         * {{comments}} give the number of comments
         * {{model.tags}} give that tags that you added
         * */
-        template:'<div class="insta__view">' +
+        template:
+        '<div class="insta__view">' +
+        '<div class="insta__content">' +
         '<div class="insta__view--image">' +
         '<a href="{{link}}" title="{{caption}}" target="_blank">' +
         '<img src="{{image}}" width="{{width}}" alt="{{caption}}" class="img-fluid"/>' +
         '</a>' +
         '</div>' +
         '<div class="insta__caption">' +
-        '<br> <strong><span class="likes"><i class="icon ion-md-heart"></i>{{likes}}</span> <span class="comments"><ion-icon name="chatbubbles"></ion-icon> {{comments}}</span></strong>' +
+        // '<br> <strong><span class="likes"><i class="icon ion-md-heart"></i>{{likes}}</span> <span class="comments"><ion-icon name="chatbubbles"></ion-icon> {{comments}}</span></strong>' +
         '<p class="insta__caption--text">{{caption}}</p>' +
-        ' </div>' +
+        '</div>' +
+        '</div>' +
         '</div>',
         // every time we load more, run this function
         after: function() {
@@ -68,14 +72,16 @@ $(document).ready(function() {
                 btnInstafeedLoad.setAttribute('disabled', 'disabled');
                 btnInstafeedLoad.style.display = "none";
             }
-        },
+            resizeAllGridItems();
+
+        }
     });
 
 
 
     // de load more button die meer foto's laten zien
     btnInstafeedLoad.addEventListener("click", function() {
-        instafeed.next()
+        instafeed.next();
     });
 
     //laat de gebruiksnaam zien.
@@ -100,6 +106,38 @@ $(document).ready(function() {
 
     instafeedProfile.run();
     instafeed.run();
-
 });
 
+window.onload = resizeAllGridItems;
+
+window.addEventListener("resize", resizeAllGridItems);
+window.addEventListener("mousemove",resizeAllGridItems);
+window.addEventListener("touchmove",resizeAllGridItems);
+window.addEventListener("scroll",resizeAllGridItems);
+
+
+function resizeGridItem(item){
+    let grid = document.getElementsByClassName("grid")[0];
+    let rowHeight = parseInt(window.getComputedStyle(grid).getPropertyValue('grid-auto-rows'));
+    let  rowGap = parseInt(window.getComputedStyle(grid).getPropertyValue('grid-row-gap'));
+    let  rowSpan = Math.ceil((item.querySelector('.insta__content').getBoundingClientRect().height+rowGap)/(rowHeight+rowGap));
+    item.style.gridRowEnd = "span "+rowSpan;
+
+
+}
+const allItems = document.getElementsByClassName("insta__view");
+for(let x=0;x<allItems.length;x++){
+    imagesLoaded( allItems[x], resizeInstance);
+}
+
+function resizeInstance(instance){
+    let item = instance.elements[0];
+    resizeGridItem(item);
+}
+
+function resizeAllGridItems(){
+    const allItems = document.getElementsByClassName("insta__view");
+    for(x=0;x<allItems.length;x++){
+        resizeGridItem(allItems[x]);
+    }
+}
